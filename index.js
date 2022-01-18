53bf8904c5a844b361c0270888611769e02ec875
Marco <marco@playwood.it>
2022-01-18 15:44:20 +0100

    make bot log

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 require('dotenv').config(); //initialize dotenv
-const Discord = require('discord.js'); //import discord.js
+const { Client, Intents } = require('discord.js');
+// const Discord = require('discord.js'); //import discord.js
 
-const client = new Discord.Client(); //create new client
+// const client = new Discord.Client(); //create new client
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+let name = 'test name'
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${name}!`);
 });
 
 //make sure this line is the last line
