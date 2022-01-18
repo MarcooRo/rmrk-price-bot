1f174ff38bd8e0513278c985e39130df6850ec12
Marco <marco@playwood.it>
2022-01-18 16:45:28 +0100

    track rmrk

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-require('dotenv').config(); //initialize dotenv
+require('dotenv').config(); 
+
 const { Client, Intents } = require('discord.js');
-// const Discord = require('discord.js'); //import discord.js
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const axios = require('axios');
 
-// const client = new Discord.Client(); //create new client
-const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
-
-let name = 'test name'
+axios.get('https://api.coingecko.com/api/v3/simple/price?ids=rmrk&vs_currencies=usd&include_24hr_change=true')
+  .then(response => {
+    console.log(response.data.rmrk);
+    console.log(response.data.rmrk.usd);
+    console.log(response.data.rmrk.usd_24h_change);
+    let coin = 'rmrk';
+    let price = response.data.rmrk.usd;
+    let move = response.data.rmrk.usd_24h_change;
+    $name = coin + ' ' + price + 'usd ' + move.toFixed(2);
+  })
+  .catch(error => {
+    console.log(error);
+  });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${name}!`);
+  client.user.setUsername($name);
+  console.log(`Logged in as ${$name}`);
 });
 
 //make sure this line is the last line
