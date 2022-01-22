b641c48f2369ad4d6c825c936f1ba8164e249f13
Marco <marco@playwood.it>
2022-01-22 15:35:40 +0100

    add glmr

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const { Client, Intents } = require('discord.js');
 const bot1 = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const bot2 = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+const bot3 = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 const axios = require('axios');
 
 function requstPriceRMRK() {
@@ -41,6 +42,24 @@ function requstPriceMOVR() {
   });
 };
 
+function requstPriceGLMR() {
+  return new Promise(resolve => {
+    axios.get('https://api.coingecko.com/api/v3/simple/price?ids=moonbeam&vs_currencies=usd&include_24hr_change=true')
+    .then(response => {
+      var coin = 'GLMR'
+      var price = response.data.moonbeam.usd
+      var move = response.data.moonbeam.usd_24h_change
+      var newActivity = coin+' $'+price+' '+move.toFixed(2)+'%'
+      console.log('----------------------')
+      console.log('update: '+newActivity)
+      resolve(newActivity)
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  });
+};
+
 async function updatePrice(){
   let resultPriceRMRK = await requstPriceRMRK();
   console.log('setActivity: '+resultPriceRMRK)
@@ -52,6 +71,10 @@ async function updatePrice(){
   console.log('----------------------')
   bot2.user.setActivity(resultPriceMOVR)
 
+  let resultPriceGLMR = await requstPriceGLMR();
+  console.log('setActivity: '+resultPriceMOVR)
+  console.log('----------------------')
+  bot3.user.setActivity(resultPriceGLMR)
 }
 
 setInterval(function (){
@@ -108,7 +131,7 @@ bot2.on('messageCreate', msg => {
   var nameCoin = messaggio.replace("!", "");
   console.log('messaggio: '+nameCoin)
   if(messaggio == '!help'){
-    var reply = 'You can ask the price in this way: !movr, !glrm, !eth'
+    var reply = 'You can ask the price in this way: !movr, !rome'
     msg.reply(reply)
   }
   if (nameCoin === 'movr') {
@@ -119,7 +142,26 @@ bot2.on('messageCreate', msg => {
       msg.reply(resultPriceMessage)
     })();
   }
-  if (nameCoin === 'glrm') {
+  if (nameCoin === 'rome') {
+    (async ()=>{
+      let resultPriceMessage = await requstPriceMessage('rome')
+      console.log('setActivity: '+resultPriceMessage)
+      console.log('----------------------')
+      msg.reply(resultPriceMessage)
+    })();
+  }
+});
+
+
+bot3.on('messageCreate', msg => {
+  var messaggio = msg.content
+  var nameCoin = messaggio.replace("!", "");
+  console.log('messaggio: '+nameCoin)
+  if(messaggio == '!help'){
+    var reply = 'You can ask the price in this way: !glrm, !eth'
+    msg.reply(reply)
+  }
+  if (nameCoin === 'glmr') {
     (async ()=>{
       let resultPriceMessage = await requstPriceMessage('moonbeam')
       console.log('setActivity: '+resultPriceMessage)
@@ -138,7 +180,7 @@ bot2.on('messageCreate', msg => {
 });
 
 
-
 //make sure this line is the last line
 bot1.login(process.env.CLIENT_TOKEN_1); //login bot 1 using token
 bot2.login(process.env.CLIENT_TOKEN_2); //login bot 2 using token
+bot3.login(process.env.CLIENT_TOKEN_3); //login bot 2 using token
